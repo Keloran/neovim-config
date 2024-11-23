4272632643f056521c4d8a848942d4d20cd011bd
Keloran <keloran@develbox.info>
2024-11-23 13:54:26 +0000

    Added a few more plugins to config

--- lua/plugins/profile.lua
+++ lua/plugins/profile.lua
@@ -1,15 +1,35 @@
 return {
-  "kurama622/profile.nvim",
-  dependencies = {
-    "3rd/image.nvim",
-  },
+  "Kurama622/profile.nvim",
+  dependencies = { "3rd/image.nvim" },
   config = function()
+    local comp = require("profile.components")
+    local win_width = vim.o.columns
     require("profile").setup({
-      avatar_path = "https://avatars.githubusercontent.com/u/200350?v=4",
-      user = "keloran"
+      avatar_path = "/home/keloran/.config/nvim/profile/image.png",
+      -- avatar position options
+      avatar_opts = {
+        avatar_width = 20,
+        avatar_height = 20,
+        avatar_x = math.floor((win_width - 20) / 2),
+        avatar_y = 7,
+        force_blank = true,   -- if true, will create some blank lines (avatar_height + avatar_y)
+      },
+
+      -- git user
+      user = "keloran",
+      git_contributions = {
+        start_week = 1, -- The minimum is 1
+        end_week = 53, -- The maximum is 53
+        empty_char = " ",
+        full_char = { "", "󰧞", "", "", "" },
+        fake_contributions = nil,
+      },
+      hide = {
+        statusline = true,
+        tabline = true,
+      },
     })
-    vim.api.nvim_set_keymap("n", "<leader>p", "<cmd>Profile<cr>", {
-      silent = true
-    })
-  end
+    vim.api.nvim_set_keymap("n", "<leader>p", "<cmd>Profile<cr>", { silent = true })
+  end,
 }
+
